e9788b1709653834002c2d1dde178900ffd66b3a
Ronaldo Almirón <rony.almiron.020@gmail.com>
2023-03-14 19:47:45 -0300

    First commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,15 @@
+
+/**
+ * Esta función me permite tomar una carta
+ * @param {Array<String>} deck un arreglo de string
+ * @returns {String} retorna la carta 
+ */
+export const pedirCarta = ( deck ) => {
+
+    if ( !deck || deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    }
+
+    return deck.pop();
+
+}
